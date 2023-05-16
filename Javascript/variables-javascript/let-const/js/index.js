df53f372acd3053cdf0bce10ef9cd1e0029021df
Sebas Backer <sa@dfvz.org>
2023-05-16 10:43:36 -0500

    javascript

---
+++ Javascript/variables-javascript/let-const/js/index.js
@@ -0,0 +1,14 @@
+var miNombre = "Juan Sebastian"
+//alert(miNombre)
+var miNombre = "Otro nombre"
+//alert(miNombre)
+
+let numero = 100
+//console.log(numero)
+numero = 50
+//console.log(numero)
+
+
+const verdadero = true;
+console.log(verdadero)
+verdadero = false
---
+++ Javascript/variables-javascript/utilizando-operadores/operaciones-con-textos/js/index.js
@@ -0,0 +1,16 @@
+// ejemplos de variables con let
+
+let nombre = "Liber"
+let apellido = "Zapata"
+let edad = 25
+let textoNumero = '10'
+let esMayorDeEdad = true
+
+let sumaEdadConTextoNumero = edad + textoNumero
+let multiplicacionEdadConTextoNumero = edad * textoNumero; 
+
+alert("Hola " + nombre + " " + apellido + " bienvenido a mi sitio tu edad es " + edad)
+console.log(typeof sumaEdadConTextoNumero)
+console.log(typeof multiplicacionEdadConTextoNumero)
+
+//NaN Not a Number
