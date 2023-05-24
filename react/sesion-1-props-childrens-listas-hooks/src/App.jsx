76f2ff14fce36808cc8d48bf5bbff6881f1e5201
Sebas Backer <sa@dfvz.org>
2023-05-23 20:48:08 -0500

    Aprendiendo Hooks

--- react/sesion-1-props-childrens-listas-hooks/src/App.jsx
+++ react/sesion-1-props-childrens-listas-hooks/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import React, { useState } from 'react'
 import Avatar from './components/Avatar'
 import Card from './components/Card'
+import Lista from './components/Lista'
 import Tanjiro from './assets/img/tanjiro.png'
 import Inosuke from './assets/img/inosuke.png'
 import Zenitsu from './assets/img/zenitsu.png'
 
 const App = () => {
-
+    const [mostrar, setMostrar] = useState(false)
+    console.log(mostrar)
 
     return(
         <div className='container'>
@@ -24,6 +27,11 @@ const App = () => {
                     tamaño={100}
                 />
             </Card>
+
+            <Lista mostrar={mostrar} />
+
+            <button onClick={() => { setMostrar(true) }}>Mostrar</button>
+            <button onClick={() => { setMostrar(false) }}>Esconder</button>
         </div>
     )
 }
